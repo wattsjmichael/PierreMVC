d08ea7691e75e9e5c0b171ad563764d9abd6a5a3
Michael Watts <wattsjmichael@gmail.com>
2020-10-02 13:18:29 -0700

    adds failing test for finding ID of Orders

--- PierreMVC.Tests/Models.Tests/OrderTests.cs
+++ PierreMVC.Tests/Models.Tests/OrderTests.cs
@@ -20,18 +20,31 @@ namespace PierreMVC.Tests
     {
       string description = "Turn Right on Barbur";
       Order newOrder = new Order(description);
-      string result =  newOrder.Description;
+      string result = newOrder.Description;
       Assert.AreEqual(description, result);
     }
     [TestMethod]
     public void GetID_OrdersInstantiatewithId_Int()
     {
       string description = "turn right on Barbur";
-       Order newOrder = new Order(description);
+      Order newOrder = new Order(description);
 
 
       int result = newOrder.Id;
       Assert.AreEqual(1, result);
+    }
+    [TestMethod]
+    public void Find_ReturnsCorrectOrder_Order()
+    {
+      string description01 = "Turn Left on Barbur";
+      string description02 = "17 Bagels";
+
+      Order newOrder1 = new Order(description01);
+      Order newOrder2 = new Order(description02);
+      
+      Order result = new Order("12 Pastries");
+      
+      Assert.AreEqual(newOrder2, result);
     }
   }
 
