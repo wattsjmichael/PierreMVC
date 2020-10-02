3ef69f1d1551d9ddb2e5ee63561445e014bf2149
Michael Watts <wattsjmichael@gmail.com>
2020-10-02 14:21:04 -0700

    Passing Vendor Name Retrieval

--- PierreMVC.Tests/Models.Tests/VendorTests.cs
+++ PierreMVC.Tests/Models.Tests/VendorTests.cs
@@ -14,5 +14,13 @@ namespace PierreMVC.Tests
       Vendor newVendor = new Vendor("Sal's Deli");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
+    [TestMethod]
+    public void GetName_ReturnsName_String()
+    {
+      string name = "Sal's Deli";
+      Vendor newVendor = new Vendor(name);
+      string result = newVendor.Name;
+      Assert.AreEqual(name, result);
+    }
   }
 }
