a2688f6a673011e5f3f2b73a5041b39b309b935d
Michael Watts <wattsjmichael@gmail.com>
2020-10-02 15:30:56 -0700

    add route for Show.cshtml that will populate Vendors Multiple Orders

---
+++ PierreMVC/Controllers/VendorsController.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using PierreMVC.Models;
+
+namespace PierreMVC.Controllers
+{
+  public class VendorsController : Controller
+  {
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(AllVendors);
+    
+    }
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName)
+    {
+      Vendor newVendor = new Vendor(vendorName);
+      return RedirectToAction("Index");
+    }
+    [HttpGet("/vendors/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor selectedVendor = Vendor.Find(id);
+      List<Order> vendorOrders = selectedVendor.Orders;
+      model.Add("Vendor", selectedVendor);
+      model.Add("Orders", vendorOrders);
+      return View(model);
+    }
+  }
+}
