4f5b34872d55010278c21f25293761bb7f9980f6
Michael Watts <wattsjmichael@gmail.com>
2020-10-02 14:48:14 -0700

    Create meaningful fail for AssociatesOrderWithVendor

--- PierreMVC.Tests/Models.Tests/VendorTests.cs
+++ PierreMVC.Tests/Models.Tests/VendorTests.cs
@@ -43,19 +43,31 @@ namespace PierreMVC.Tests
       string name02 = "Ritchies Diner";
       Vendor newVendor1 = new Vendor(name01);
       Vendor newVendor2 = new Vendor(name02);
-      List<Vendor> newList = new List<Vendor> { newVendor1, newVendor2};
+      List<Vendor> newList = new List<Vendor> { newVendor1, newVendor2 };
       List<Vendor> result = Vendor.GetAll();
       CollectionAssert.AreEqual(newList, result);
     }
-      [TestMethod]
-  public void Find_ReturnsCorrectVendor_Vendor()
-  {
-    string name01 = "Sal's Deli";
-    string name02 = "Ritchies Diner";
-    Vendor newVendor1 = new Vendor(name01);
-    Vendor newVendor2 = new Vendor(name02);
-    Vendor result = Vendor.Find(2);
-    Assert.AreEqual(newVendor2, result);
-  }
+    [TestMethod]
+    public void Find_ReturnsCorrectVendor_Vendor()
+    {
+      string name01 = "Sal's Deli";
+      string name02 = "Ritchies Diner";
+      Vendor newVendor1 = new Vendor(name01);
+      Vendor newVendor2 = new Vendor(name02);
+      Vendor result = Vendor.Find(2);
+      Assert.AreEqual(newVendor2, result);
+    }
+    [TestMethod]
+    public void AddOrder_AssociatesOrderwithVendor_OrderList()
+    {
+      string description = "12 Bagels";
+      Order newOrder = new Order(description);
+      List<Order> newList = new List<Order> {newOrder};
+      string name = "Sal's Diner";
+      Vendor newVendor = new Vendor(name);
+      newVendor.AddOrder(newOrder);
+      List<Order> result = newVendor.Orders;
+      CollectionAssert.AreEqual(newList, result);
+    }
   }
 }
--- PierreMVC/Models/Vendor.cs
+++ PierreMVC/Models/Vendor.cs
@@ -30,5 +30,10 @@ namespace PierreMVC.Models
     {
       return _instances[searchId -1];
     }
+
+    public void AddOrder(Order order)
+    {
+      
+    }
   }
 }
