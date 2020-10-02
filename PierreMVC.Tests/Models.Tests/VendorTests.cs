77f64656131dd972e2ecdb720cd432fd7b29ca4e
Michael Watts <wattsjmichael@gmail.com>
2020-10-02 14:33:26 -0700

    adds Meanigful failing test for returning all Vendor Objects

--- PierreMVC.Tests/Models.Tests/VendorTests.cs
+++ PierreMVC.Tests/Models.Tests/VendorTests.cs
@@ -36,5 +36,16 @@ namespace PierreMVC.Tests
       int result = newVendor.Id;
       Assert.AreEqual(1, result);
     }
+    [TestMethod]
+    public void GetAll_ReturnsAllVendorObjects_VendorList()
+    {
+      string name01 = "Sal's Deli";
+      string name02 = "Ritchies Diner";
+      Vendor newVendor1 = new Vendor(name01);
+      Vendor newVendor2 = new Vendor(name02);
+      List<Vendor> newList = new List<Vendor> { newVendor1, newVendor2};
+      List<Vendor> result = Vendor.GetAll();
+      CollectionAssert.AreEqual(newList, result);
+    }
   }
 }
